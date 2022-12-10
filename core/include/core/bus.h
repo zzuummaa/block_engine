3ae496802714fb952ef23c98f4121cef57ec33da
Stepan <stepannk@mail.ru>
2022-12-10 20:39:13 +0300

    Clear Bus implementation

--- core/include/core/bus.h
+++ core/include/core/bus.h
@@ -14,30 +14,19 @@
 
 struct Bus {
     TypeInfo type_info;
-    std::vector<char> data_;
-    std::function<void()> destroyer;
+    std::shared_ptr<void> data_;
 
     Bus(const Bus& other) = default;
     Bus(Bus&&) = default;
 
     template<typename TDataType>
-    explicit Bus(const TDataType& value) : type_info(make_type_info<TDataType>()), data_(sizeof(value)) {
-        *reinterpret_cast<TDataType*>(data_.data()) = value;
-        destroyer = [this](){
-            auto& data_value = data_unchecked<TDataType>();
-            data_value.~TDataType();
-        };
-    }
-
-    ~Bus() {
-        if (destroyer) destroyer();
+    explicit Bus(const TDataType& value) : type_info(make_type_info<TDataType>()) {
+        data_ = std::make_shared<TDataType>(value);
     }
 
     template<typename TDataType>
     TDataType& data_unchecked() {
-        auto* start_data = data_.data();
-        auto* typed_data = reinterpret_cast<TDataType*>(start_data);
-        return *typed_data;
+        return *std::static_pointer_cast<TDataType>(data_);
     }
 
     template<typename TDataType>
